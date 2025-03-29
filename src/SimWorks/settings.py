0a62e3027a4f1fceac00b519bdf58080bc79e2b9
Tyler J <tyler@jackfruit.me>
2025-03-29 19:43:48 -0400

    initial commit

--- src/SimWorks/settings.py
+++ src/SimWorks/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR.parent / "staticfiles"
+STATIC_ROOT = BASE_DIR.parent / "static"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
