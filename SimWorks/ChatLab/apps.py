346b46b8f2c3f9cd4f2ed6614ba3cf1c3a3fe971
Tyler J <tyler@jackfruit.me>
2025-04-13 11:42:10 -0400

    Add signal for post_save mode logging in ChatLab

--- SimWorks/ChatLab/apps.py
+++ SimWorks/ChatLab/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class ChatLabConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = 'django.db.models.BigAutoField'
     name = "ChatLab"
+
+    def ready(self):
+        import ChatLab.signals
---
+++ SimWorks/ChatLab/signals.py
@@ -0,0 +1,26 @@
+import logging
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.apps import apps
+
+logger = logging.getLogger(__name__)
+
+def log_model_save(sender, instance, created, **kwargs):
+    class_name = sender.__name__
+    object_id = getattr(instance, 'id', 'N/A')
+    object_name = str(instance)
+
+    status = "CREATED" if created else "MODIFIED"
+    msg = f"[{class_name}] {object_name} (ID: {object_id}) was {status}."
+
+    if logger.isEnabledFor(logging.DEBUG):
+        debug_info = f"Full object data: {vars(instance)}"
+        msg += f" | DEBUG: {debug_info}"
+
+    logger.info(msg)
+
+# Connect to all models in ChatLab
+chatlab_models = apps.get_app_config("ChatLab").get_models()
+
+for model in chatlab_models:
+    post_save.connect(log_model_save, sender=model, dispatch_uid=f"log_save_{model.__name__}")
