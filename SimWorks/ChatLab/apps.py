11ab4d7c95dd851bf78b52840cb2f4d538eaed68
Tyler J <tyler@jackfruit.me>
2025-04-20 12:29:33 -0400

    add chatlab_history and register to simcore.Simulation

--- SimWorks/ChatLab/apps.py
+++ SimWorks/ChatLab/apps.py
@@ -1,8 +1,24 @@
 from django.apps import AppConfig
 
 class ChatLabConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = "chatlab"
 
     def ready(self):
         import chatlab.signals
+        from simcore.history_registry import register_history_provider
+        from .models import Message
+
+        def chatlab_history(simulation):
+            return [
+                {
+                    "source": msg._meta.app_label,
+                    "timestamp": msg.timestamp,
+                    "role": msg.role,
+                    "sender": msg.display_name or str(msg.sender),
+                    "text": msg.text,
+                }
+                for msg in Message.objects.filter(simulation=simulation).order_by("timestamp")
+            ]
+
+        register_history_provider("chatlab", chatlab_history)
+
