1ca1f0fdc0fb74066107da5ebbb4da26d0aa854e
Tyler J <71736809+tylerj117@users.noreply.github.com>
2025-09-14 16:02:15 -0400

    Use Strawberry Info type in middleware

--- SimWorks/config/middleware.py
+++ SimWorks/config/middleware.py
@@ -1,16 +1,16 @@
 from graphql import GraphQLError
-from graphql import GraphQLResolveInfo as ResolveInfo
+from strawberry.types import Info
 
 
 class RequireApiPermissionMiddleware:
-    def resolve(self, next, root, info: ResolveInfo, **kwargs):
+    def resolve(self, next_, root, info: Info, **kwargs):
         # Whitelist any queries/mutations that don't require auth
         open_operations = {"tokenAuth", "verifyToken", "refreshToken"}
 
         if info.field_name in open_operations:
-            return next(root, info, **kwargs)
+            return next_(root, info, **kwargs)
 
         user = info.context.user
         if not user or not user.has_perm("core.read_api"):
             raise GraphQLError("Permission denied: `read_api` scope required.")
-        return next(root, info, **kwargs)
+        return next_(root, info, **kwargs)
