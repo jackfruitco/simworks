f1b9c04060260f621f35ab4e637e796718c0dac2
Tyler J <tyler@jackfruit.me>
2026-04-11 12:35:05 -0400

    temp(asgi): remove unused AllowedHostsOriginValidator wrapper

--- SimWorks/config/asgi.py
+++ SimWorks/config/asgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -24,8 +23,8 @@ from apps.common.ws_auth import SessionOrJWTAuthMiddlewareStack  # noqa: E402
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            SessionOrJWTAuthMiddlewareStack(URLRouter(chatlab_ws + core_ws))
+        "websocket": SessionOrJWTAuthMiddlewareStack(
+            URLRouter(chatlab_ws + core_ws)
         ),
     }
 )
